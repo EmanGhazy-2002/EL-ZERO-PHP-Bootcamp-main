f6648054dbd0ed0b208e150ac26f0d699e9030be
Eman Ghazy <ghazyeman220@gmail.com>
2024-10-29 18:31:23 +0300

    Update assignment-06.php

--- week-04-Operators[20-29]/assignment-06.php
+++ week-04-Operators[20-29]/assignment-06.php
@@ -1,1 +1,25 @@
 <?php
+
+$a = "Elzero";
+$b = "Web";
+$c = "School";
+
+// Method One
+//$d = $a.' '.$b.' '.$c;
+
+
+// Method Two
+//$d = $a.=' '.$b.' '.$c.' ' ;
+
+
+// Method Three
+// $d = $a $b $c;
+
+
+// Method Four
+//$d = sprintf("%s %s %s" , $a,$b,$c);
+
+// Method Five
+//$d = implode(" " , [$a,$b,$c]);
+
+//echo $d; // Elzero Web School
